3151fc64063bd1f5dbb64266a987f1a462d6a9ed
mercedes sandu <mxrcedes@gmail.com>
2023-09-28 15:14:33 -0500

    course selection

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,48 +1,55 @@
-import { CardContent, Divider, Typography } from "@mui/material";
+import { CardActionArea, CardContent, Divider, Typography } from "@mui/material";
 import { StyledCard, StyledCardBox } from "../styles/StyledComponents";
 
-function CourseCard({ course }) {
-  return (
-    <StyledCard>
-      <CardContent>
-        <Typography 
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{ padding: 1 }}
-        >
-          {course.term} CS {course.number}
-        </Typography>
-        <StyledCardBox>
-          <Typography
-          variant="body1"
-          color="text.primary"
-          sx={{ 
-            padding: 1,
-            fontSize: "18px",
-            lineHeight: "1.75",
-          }}
-        >
-            {course.title}
+function CourseCard({ course, isSelected, onCourseCardClick, courseId }) {return (
+    <StyledCard
+      onClick={() => onCourseCardClick(courseId)}
+      sx={{
+        backgroundColor: isSelected ? "#90caf9" : "#ffffff",
+        borderColor: isSelected ? "#42a5f5" : "#cccccc",
+      }}
+    >
+      <CardActionArea>
+        <CardContent>
+          <Typography 
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{ padding: 1 }}
+          >
+            {course.term} CS {course.number}
           </Typography>
-        </StyledCardBox>
-        <Divider 
-          sx={{ 
-            background: "#eeeeee",
-            borderBottomWidth: "2px",
-          }} 
-        />
-        <Typography
-          variant="body1"
-          color="text.primary"
-          sx={{ 
-            padding: 1,
-            fontSize: "18px",
-          }}
-        >
-          {course.meets}
-        </Typography>
-      </CardContent>
+          <StyledCardBox>
+            <Typography
+            variant="body1"
+            color="text.primary"
+            sx={{ 
+              padding: 1,
+              fontSize: "18px",
+              lineHeight: "1.75",
+            }}
+          >
+              {course.title}
+            </Typography>
+          </StyledCardBox>
+          <Divider 
+            sx={{ 
+              background: "#eeeeee",
+              borderBottomWidth: "2px",
+            }} 
+          />
+          <Typography
+            variant="body1"
+            color="text.primary"
+            sx={{ 
+              padding: 1,
+              fontSize: "18px",
+            }}
+          >
+            {course.meets}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </StyledCard>
   );
 }
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,13 +1,31 @@
 import CourseCard from './CourseCard';
 import { Grid } from '@mui/material';
 import { StyledGrid } from '../styles/StyledComponents';
+import { useState } from 'react';
 
 function CourseList({ courses }) {
+    const [selectedCourseList, setSelectedCourseList] = useState([]);
+
+    const handleCourseCardClick = (courseId) => {
+        if (selectedCourseList.includes(courseId)) {
+            setSelectedCourseList((prevList) =>
+                prevList.filter((id) => id !== courseId)
+        );
+        } else {
+            setSelectedCourseList((prevList) => [...prevList, courseId]);
+        }
+    };
+
     return (
         <StyledGrid container rowSpacing={3} columnSpacing={3}>
-            {Object.values(courses).map(course => (
-                <Grid item>
-                    <CourseCard key={course.id} course={course} />
+            {Object.entries(courses).map(([key, value]) => (
+                <Grid item key={key}>
+                    <CourseCard
+                        course={value}
+                        isSelected={selectedCourseList.includes(key)}
+                        onCourseCardClick={handleCourseCardClick}
+                        courseId={key}
+                    />
                 </Grid>
             ))}
         </StyledGrid>

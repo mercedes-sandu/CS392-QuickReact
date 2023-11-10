1584084639b5741fe5f47dcd1f88b1d1e6ac5c34
mercedes sandu <mxrcedes@gmail.com>
2023-11-10 12:56:03 -0600

    fix vite config file

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,29 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-  }
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /(src)\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { '.js': 'jsx' },
+      plugins: [
+        {
+          name: 'load-js-files-as-jsx',
+          setup(build) {
+            build.onLoad(
+              { filter: /(src)\/.*\.js$/ },
+              async (args) => ({
+                loader: 'jsx',
+                contents: await fs.readFile(args.path, 'utf8'),
+              })
+            );
+          },
+        },
+      ],
+    },
+  },
 });
